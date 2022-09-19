571c93f9727dc080419fe45abea742171e18d5bd
salvatoreromeo <info@salvatoreromeo.com>
2022-09-19 16:14:52 +0200

    Add browserbot website v1.0

---
+++ packages/browserbot.io/tailwind.config.js
@@ -0,0 +1,25 @@
+const tw = require('@butopen/design-system/tailwind.config');
+
+module.exports = {
+  mode: 'jit',
+  prefix: '',
+  important: true,
+  content: ['./src/**/*.{html,ts,scss,js,md}'],
+  theme: {
+    extend: {
+      ...tw.theme.extend,
+      colors: {
+        ...tw.theme.extend.colors,
+        bbp: 'var(--bb-primary-color)',
+        bbs: 'var(--bb-secondary-color)'
+      },
+      scale: {
+        200: '2',
+        300: '3'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  }
+};
