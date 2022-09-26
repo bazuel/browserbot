4e093429ac9179fe3e41e941ae0f295162993d65
salvatoreromeo <info@salvatoreromeo.com>
2022-09-26 16:51:02 +0200

    add sid to extension monitoring and make tab dyncamic for the current event

--- packages/browserbot-extension/src/chrome/background.ts
+++ packages/browserbot-extension/src/chrome/background.ts
@@ -4,17 +4,21 @@ import { getCurrentTab } from './current-tab.util';
 import { isRecording, setRecording } from './recording.state';
 import { uploadEvents } from './upload.api';
 
-const session = { tab: 0 };
 let cookieDetailsEvent: any = {};
 let events: BLEvent[] = [];
 
+let sid = await chrome.storage.local.get('sid');
+if (!sid) {
+  await chrome.storage.local.set({ sid: new Date().getTime() });
+}
+
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-  const tab = await getCurrentTab();
-  const url = tab.url;
   if (request.messageType == 'popup-open') {
     disableRecordingIcon();
     const recording = await isRecording();
     if (recording) {
+      const tab = await getCurrentTab();
+      const url = tab.url;
       await setRecording(false);
       await chrome.tabs.sendMessage(tab.id!, { messageType: 'recording-ended' });
       const res = await uploadEvents(url!, events);
@@ -25,17 +29,18 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
     if (tabId) {
       await setRecording(true);
       await chrome.tabs.reload(tabId);
-      session.tab = tabId ?? Date.now();
       enableRecordingIcon();
     }
   } else {
     if (request.messageType == 'session-event') {
       const { messageType, ...r } = request;
-      events.push({ ...r, timestamp: Date.now(), url, ...session });
+      const tab = await getCurrentTab()
+      const url = tab?.url ?? ""
+      events.push({ ...r, timestamp: Date.now(), url, sid, tab: tab.id });
 
       if (r.name == 'referrer') {
         cookieDetailsEvent = await getCookiesFromDomain(url);
-        events.push({ ...cookieDetailsEvent, timestamp: Date.now(), url, ...session });
+        events.push({ ...cookieDetailsEvent, timestamp: Date.now(), url, sid, tab: tab.id });
       }
     }
     sendResponse({});
