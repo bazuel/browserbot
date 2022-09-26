4e2c39a944bf2fb7b1ff18daa4b091a7b3ee38db
salvatoreromeo <info@salvatoreromeo.com>
2022-09-26 16:51:18 +0200

    always record on active tab on active window

--- packages/browserbot-extension/src/chrome/current-tab.util.ts
+++ packages/browserbot-extension/src/chrome/current-tab.util.ts
@@ -1,5 +1,5 @@
 export async function getCurrentTab() {
-    let queryOptions = { active: true }; //lastFocusedWindow: true };
+    let queryOptions = { active: true, currentWindow: true }; //lastFocusedWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
